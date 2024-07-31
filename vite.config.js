543db921e6e5b7f4e4b936b9b9c60a3b880bf229
Francois Lemieux <hello@frvncois.com>
2024-07-31 15:45:55 -0400

    url change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dev/', // Update this line to match the subdirectory
+  base: './', // Add this line
   plugins: [
     vue(),
   ],
